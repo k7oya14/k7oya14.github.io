e5e8ca718a92be9977a07a8e1d290b576dfd47b1
Koya Saka <k7oya14@gmail.com>
2024-05-19 13:23:56 +0900

    chore: add output configuration to next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,7 @@ const nextConfig = {
   // Configure `pageExtensions` to include markdown and MDX files
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
   // Optionally, add any other Next.js config below
+  output: "export",
 }
 
 const withMDX = createMDX({
