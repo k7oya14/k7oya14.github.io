4a306e4bd56af2fcdde56a50c2dacc44f49d3ead
Koya Saka <k7oya14@gmail.com>
2024-05-20 00:19:20 +0900

    Got it done for now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
-import Portfolio from "@/markdown/portfolio.mdx";
 import SkillsAndInterests from "@/markdown/skills-interests.mdx";
+import Experience from "@/markdown/experience.mdx";
+import Portfolio from "@/markdown/portfolio.mdx";
 
 export default function Page() {
   return (
@@ -7,16 +8,19 @@ export default function Page() {
       <h1 className="mb-8 text-4xl font-bold tracking-tighter">My Portfolio</h1>
       <p className="mb-4">
         <b>
-          My name is Koya Saka, an undergraduate student at the Waseda
-          University.&nbsp;
+          My name is Koya Saka, an undergraduate student at Waseda University.
+          <br />
         </b>
         I major in Electronic and Physical Systems, i.e., Electrical Engineering
-        (<b>EE</b>), and minor in Computer Science (<b>CS</b>). I have a strong
-        passion for the fusion of hardware and software, from circuit design to
-        rich web/application development.
+        (EE), and minor in Computer Science (CS). I have a strong passion for
+        the fusion of hardware and software, from circuit design to rich
+        web/apps development.
       </p>
       <section className="my-8 prose dark:prose-invert">
         <SkillsAndInterests />
+      </section>
+      <section className="my-8 prose dark:prose-invert">
+        <Experience />
       </section>
       <section className="my-8 prose dark:prose-invert">
         <Portfolio />
